87a5f0ec1463442f03617f157d28873b1a68bbf4
Martin Ring <ring.martin@gmail.com>
2020-07-23 00:30:25 -0300

    final styles

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const burgerIcon = document.getElementById('toggle');
+const navList = document.getElementById('nav-list');
+
+burgerIcon.addEventListener('click', () => navList.classList.toggle('hide'));
